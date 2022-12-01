8e6f56ba5f89d78dc7c83c2d1ebd4fb9afa26784
DESKTOP-UTJD32H\One o dem red hats <maxan_reimblad_3541@hotmail.com>
2022-12-01 16:30:52 +0100

    Less try

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HELLO THERE MF!");
+        System.out.println();
+        System.out.println("And the room started to sing!");
     }
 }
